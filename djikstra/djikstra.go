1fd3e96a99e7b1a3f951676569165ec9b4416a88
Ferozaffs <zmass@ZMASS>
2023-10-04 19:06:14 +0200

    Added djikstra in Go

---
+++ djikstra/djikstra.go
@@ -0,0 +1,58 @@
+package djikstra
+
+type edge struct {
+	Vertex *vertex
+	Weight float64
+}
+
+type vertex struct {
+	Edges []edge
+}
+
+func addEdge(Src *vertex, Dest *vertex, Weight float64) {
+	Src.Edges = append(Src.Edges, edge{Vertex: Dest, Weight: Weight})
+}
+
+func pathfind(Vertex []vertex, Src *vertex, Dest *vertex) []*vertex {
+	queue := []*vertex{}
+	queue = append(queue, Src)
+
+	dist := make(map[*vertex]edge)
+	dist[Src] = edge{nil, 0.0}
+
+	for {
+		if len(queue) == 0 {
+			break
+		}
+
+		u := queue[0]
+		queue = queue[1:]
+
+		for _, e := range u.Edges {
+			v, exists := dist[e.Vertex]
+			if exists != true {
+				dist[e.Vertex] = edge{u, dist[u].Weight + e.Weight}
+				queue = append(queue, e.Vertex)
+			} else {
+				if v.Weight > dist[u].Weight+e.Weight {
+					dist[e.Vertex] = edge{u, dist[u].Weight + e.Weight}
+					queue = append(queue, e.Vertex)
+				}
+			}
+		}
+	}
+
+	path := []*vertex{}
+	path = append(path, Dest)
+	v := Dest
+	for {
+		_, exists := dist[v]
+		if dist[v].Vertex == nil || exists != true {
+			break
+		}
+		v = dist[v].Vertex
+		path = append(path, v)
+	}
+
+	return path
+}
---
+++ djikstra/djikstra_test.go
@@ -0,0 +1,54 @@
+package djikstra
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestDjikstraSimple(t *testing.T) {
+	vertices := make([]vertex, 4)
+
+	addEdge(&vertices[0], &vertices[1], 1.5)
+	addEdge(&vertices[0], &vertices[2], 2.0)
+	addEdge(&vertices[1], &vertices[3], 0.5)
+	addEdge(&vertices[2], &vertices[3], 1.0)
+
+	path := pathfind(vertices, &vertices[0], &vertices[3])
+	if len(path) != 3 {
+		t.Fatalf("Path is not correct length: %d expected 3", len(path))
+	}
+
+	correctPath := []*vertex{&vertices[0], &vertices[1], &vertices[3]}
+	for _, p := range path {
+		if slices.Contains(correctPath, p) == false {
+			t.Fatalf("Path is invalid")
+		}
+	}
+}
+
+func TestDjikstraComplex(t *testing.T) {
+	vertices := make([]vertex, 10)
+
+	addEdge(&vertices[0], &vertices[1], 1.5)
+	addEdge(&vertices[0], &vertices[2], 2.0)
+	addEdge(&vertices[1], &vertices[3], 0.5)
+	addEdge(&vertices[1], &vertices[4], 1.0)
+	addEdge(&vertices[2], &vertices[5], 2.0)
+	addEdge(&vertices[2], &vertices[6], 1.5)
+	addEdge(&vertices[3], &vertices[7], 0.5)
+	addEdge(&vertices[4], &vertices[8], 2.0)
+	addEdge(&vertices[5], &vertices[7], 0.5)
+	addEdge(&vertices[6], &vertices[8], 1.5)
+
+	path := pathfind(vertices, &vertices[0], &vertices[8])
+	if len(path) != 4 {
+		t.Fatalf("Path is not correct length: %d expected 4", len(path))
+	}
+
+	correctPath := []*vertex{&vertices[0], &vertices[1], &vertices[4], &vertices[8]}
+	for _, p := range path {
+		if slices.Contains(correctPath, p) == false {
+			t.Fatalf("Path is invalid")
+		}
+	}
+}
