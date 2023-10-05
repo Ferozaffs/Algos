92c73c0126f4f991c44b34ba12e13e180e23a26c
Ferozaffs <zmass@ZMASS>
2023-10-05 19:40:34 +0200

    recursive maze solver

--- RecursiveMazeSolver/recurMaze.go
+++ RecursiveMazeSolver/recurMaze.go
@@ -5,10 +5,59 @@ type Coordinate struct {
 	y int
 }
 
-func CheckCoordinate(c Coordinate, maze *[][]int, path *[]Coordinate)
+func CheckCoordinate(c Coordinate, maze [][]int, visited [][]bool, path *[]Coordinate) bool {
+	directions := [4]Coordinate{
+		{x: 1, y: 0},
+		{x: 0, y: 1},
+		{x: -1, y: 0},
+		{x: 0, y: -1},
+	}
+
+	if c.x < 0 || c.y < 0 || c.x >= len(maze) || c.y >= len(maze[0]) {
+		return false
+	} else if maze[c.x][c.y] == 1 {
+		return false
+	} else if visited[c.x][c.y] == true {
+		return false
+	} else if maze[c.x][c.y] == 6 {
+		return true
+	}
+
+	visited[c.x][c.y] = true
+
+	for _, d := range directions {
+		newCoord := c
+		newCoord.x += d.x
+		newCoord.y += d.y
+
+		if CheckCoordinate(newCoord, maze, visited, path) {
+			*path = append(*path, newCoord)
+			return true
+		}
+	}
+
+	return false
+}
 
 func SolveMaze(maze [][]int) []Coordinate {
 	path := []Coordinate{}
+	visited := [][]bool{}
+
+	start := Coordinate{}
+
+	for x, row := range maze {
+		visited = append(visited, []bool{})
+		for y, val := range row {
+			if val == 3 {
+				start.x = x
+				start.y = y
+			}
+			visited[x] = append(visited[x], false)
+		}
+	}
+
+	CheckCoordinate(start, maze, visited, &path)
+	path = append(path, start)
 
 	return path
 }
--- RecursiveMazeSolver/recurMaze_test.go
+++ RecursiveMazeSolver/recurMaze_test.go
@@ -5,17 +5,32 @@ import (
 )
 
 func TestMazeSimple(t *testing.T) {
-	maze := [3][4]int{
+	maze := [][]int{
 		{1, 1, 1, 3},
 		{0, 0, 0, 0},
 		{6, 1, 1, 1},
 	}
 
-	SolveMaze(maze)
+	path := SolveMaze(maze)
+
+	solution := [6]Coordinate{
+		{x: 2, y: 0},
+		{x: 1, y: 0},
+		{x: 1, y: 1},
+		{x: 1, y: 2},
+		{x: 1, y: 3},
+		{x: 0, y: 3},
+	}
+
+	for i, c := range solution {
+		if c.x != path[i].x || c.y != path[i].y {
+			t.Fatalf("Path is invalid, expected %d,%d got %d,%d", c.x, c.y, path[i].x, path[i].y)
+		}
+	}
 }
 
 func TestMazeComplex(t *testing.T) {
-	maze := [8][8]int{
+	maze := [][]int{
 		{1, 1, 1, 1, 1, 1, 0, 0},
 		{0, 1, 0, 0, 0, 0, 0, 1},
 		{0, 1, 0, 1, 0, 3, 0, 1},
@@ -26,5 +41,35 @@ func TestMazeComplex(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0},
 	}
 
-	SolveMaze(maze)
+	path := SolveMaze(maze)
+
+	solution := []Coordinate{
+		{x: 4, y: 3},
+		{x: 5, y: 3},
+		{x: 5, y: 2},
+		{x: 5, y: 1},
+		{x: 5, y: 0},
+		{x: 6, y: 0},
+		{x: 7, y: 0},
+		{x: 7, y: 1},
+		{x: 7, y: 2},
+		{x: 7, y: 3},
+		{x: 7, y: 4},
+		{x: 7, y: 5},
+		{x: 7, y: 6},
+		{x: 7, y: 7},
+		{x: 6, y: 7},
+		{x: 5, y: 7},
+		{x: 4, y: 7},
+		{x: 3, y: 7},
+		{x: 3, y: 6},
+		{x: 2, y: 6},
+		{x: 2, y: 5},
+	}
+
+	for i, c := range solution {
+		if c.x != path[i].x || c.y != path[i].y {
+			t.Fatalf("Path is invalid, expected %d,%d got %d,%d", c.x, c.y, path[i].x, path[i].y)
+		}
+	}
 }
